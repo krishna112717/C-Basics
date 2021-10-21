21e5c668e2c1d187b29295503b8b9a5659f56d18
krishna112717 <92074996+krishna112717@users.noreply.github.com>
2021-10-21 10:44:15 +0530

    Add files via upload

---
+++ krishna.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+printf("krishna!\nk\nr\ni\ns\nh\nn\na");
+}
+
---
+++ self details.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+void main()
+{printf("selfdetails\nRoll.no:20MH5A0330\nName:Yasodha krishna\nBranch:Machanical\nCollage:Adithy collage of engineering");
+}
