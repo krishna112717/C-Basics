4a33c1922793da8d495281c7090980daafd9a0ea
krishna112717 <92074996+krishna112717@users.noreply.github.com>
2021-12-16 10:45:35 +0530

    Add files via upload

---
+++ adding.c
@@ -0,0 +1,16 @@
+/* add two integers*/
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	float number1, number2, sum;
+	
+	//Initialization
+	number1=8.2;
+	number2=4.8;
+	
+	//calclation
+	sum=number1+number2;//10+20=30
+	//output
+	printf("sum of %f and %f is %f",sum);
+}
