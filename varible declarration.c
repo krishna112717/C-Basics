1f1f2629333c8170b99f2622790034fb67721060
krishna112717 <92074996+krishna112717@users.noreply.github.com>
2021-12-16 10:48:43 +0530

    Add files via upload

---
+++ varible declarration.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//varible Decleration
+	int age;
+	float cgpa;
+	char initial;
+	 
+	 
+	//Intialization
+	age= 21;
+	cgpa= 6.2;
+	initial= 'K';
+	
+	//Output
+	printf("Age of the student is %d\n", age);
+	printf("Cgpa of the student is %f\n", cgpa);
+	printf("student surname start with %c\n", initial);
+}
